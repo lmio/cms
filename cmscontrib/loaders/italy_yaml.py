c1b4d3b7d555c6e98c5ca35e4dcfe571251b8e0d
Vytis Banaitis <vytis.banaitis@gmail.com>
2014-04-19 17:52:13 +0300

    Import multiple statements per task.

--- cmscontrib/loaders/italy_yaml.py
+++ cmscontrib/loaders/italy_yaml.py
@@ -353,19 +353,38 @@ class YamlLoader(ContestLoader, TaskLoader, UserLoader, TeamLoader):
             primary_language = load(conf, None, "primary_language")
             if primary_language is None:
                 primary_language = 'it'
-            paths = [os.path.join(self.path, "statement", "statement.pdf"),
-                     os.path.join(self.path, "testo", "testo.pdf")]
-            for path in paths:
-                if os.path.exists(path):
-                    digest = self.file_cacher.put_file_from_path(
-                        path,
-                        "Statement for task %s (lang: %s)" %
-                        (name, primary_language))
-                    break
+            statement_languages = load(conf, None, "statement_languages")
+            if statement_languages is None:
+                paths = [os.path.join(self.path, "statement", "statement.pdf"),
+                         os.path.join(self.path, "testo", "testo.pdf")]
+                for path in paths:
+                    if os.path.exists(path):
+                        digest = self.file_cacher.put_file_from_path(
+                            path,
+                            "Statement for task %s (lang: %s)" %
+                            (name, primary_language))
+                        break
+                else:
+                    logger.critical("Couldn't find any task statement, aborting.")
+                    sys.exit(1)
+                args["statements"] = [Statement(primary_language, digest)]
             else:
-                logger.critical("Couldn't find any task statement, aborting.")
-                sys.exit(1)
-            args["statements"] = [Statement(primary_language, digest)]
+                statements = []
+                for language in statement_languages:
+                    path = os.path.join(
+                        self.path,
+                        "statement",
+                        "statement-%s.pdf" % (language))
+                    if os.path.exists(path):
+                        digest = self.file_cacher.put_file_from_path(
+                            path,
+                            "Statement for task %s (lang: %s)" % (name,
+                                                                  language))
+                        statements.append(Statement(language, digest))
+                    else:
+                        logger.warning("Statement for language %s was not found",
+                                       language)
+                args["statements"] = statements
 
             args["primary_statements"] = '["%s"]' % (primary_language)
 
@@ -764,7 +783,8 @@ class YamlLoader(ContestLoader, TaskLoader, UserLoader, TeamLoader):
         files.append(os.path.join(self.path, "gen", "GEN"))
 
         # Statement
-        files.append(os.path.join(self.path, "statement", "statement.pdf"))
+        for filename in os.listdir(os.path.join(self.path, "statement")):
+            files.append(os.path.join(self.path, "statement", filename))
         files.append(os.path.join(self.path, "testo", "testo.pdf"))
 
         # Managers
