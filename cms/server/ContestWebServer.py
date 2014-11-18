88223092fd5786b0358d0d117e34b1d2b0e35f12
Vytis Banaitis <vytis.banaitis@gmail.com>
2014-11-18 02:32:31 +0200

    Modified password generation function

--- cms/server/ContestWebServer.py
+++ cms/server/ContestWebServer.py
@@ -600,10 +600,9 @@ class RegisterHandler(BaseHandler):
                              random.randint(0, 9999))
 
     def generate_password(self):
-        consonants = "bcdfghjklmnpqrstvwxyz"
-        vowels = "aeiou"
-        return "".join(random.choice(consonants) + random.choice(vowels)
-                       for _i in xrange(4))
+        chars = "abcdefghijkmnopqrstuvwxyz23456789"
+        return "".join(random.choice(chars)
+                       for _i in xrange(8))
 
 
 class StartHandler(BaseHandler):
