e4d87cf484ad08c821ba6fbdf46e01b8457b1ea8
Andrius Žiūkas <andriusa2@gmail.com>
2017-12-05 22:18:24 +0000

    Fix client reloading causing DoS during contest start.

--- cms/server/contest/static/cws_utils.js
+++ cms/server/contest/static/cws_utils.js
@@ -37,6 +37,7 @@ CMS.CWSUtils = function(url_root, timestamp, timezoned_timestamp,
     this.phase = phase;
     this.remaining_div = null;
     this.unread_count = 0;
+    this.is_reloading = false;
 
     // Ask permission for desktop notifications
     if ("Notification" in window) {
@@ -208,8 +209,9 @@ CMS.CWSUtils.prototype.update_time = function(usaco_like_contest) {
     switch (this.phase) {
     case -2:
         // Contest hasn't started yet.
-        if (server_time >= this.current_phase_end) {
+        if (server_time >= this.current_phase_end && !this.is_reloading) {
             window.location.href = this.url_root + "/";
+            this.is_reloading = true;
         }
         $("#countdown_label").text(
             $("#translation_until_contest_starts").text());
