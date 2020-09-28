656aa9b3e9c8db375474aa8300f97c2242d5f574
Andrey Vihrov <andrey.vihrov@gmail.com>
2020-09-28 08:48:44 +0300

    Add C++17 programming language
    (cherry picked from commit 62623291cddedd263a184217eca861b173f977b0)

--- cmstestsuite/Tests.py
+++ cmstestsuite/Tests.py
@@ -49,6 +49,7 @@ from cmstestsuite.Test import Test, CheckOverallScore, CheckCompilationFail, \
 
 
 LANG_CPP = "C++11 / g++"
+LANG_CPP17 = "C++17 / g++"
 LANG_C = "C11 / gcc"
 LANG_HS = "Haskell / ghc"
 LANG_JAVA = "Java / JDK"
@@ -58,13 +59,15 @@ LANG_PYTHON = "Python 2 / CPython"
 LANG_RUST = "Rust"
 LANG_C_SHARP = "C# / Mono"
 ALL_LANGUAGES = (
-    LANG_CPP, LANG_C, LANG_HS, LANG_JAVA, LANG_PASCAL, LANG_PHP, LANG_PYTHON,
-    LANG_RUST, LANG_C_SHARP
+    LANG_CPP, LANG_CPP17, LANG_C, LANG_HS, LANG_JAVA, LANG_PASCAL,
+    LANG_PHP, LANG_PYTHON, LANG_RUST, LANG_C_SHARP
 )
-NON_INTERPRETED_LANGUAGES = (LANG_C, LANG_CPP, LANG_PASCAL)
+NON_INTERPRETED_LANGUAGES = (
+    LANG_C, LANG_CPP, LANG_CPP17, LANG_PASCAL
+)
 COMPILED_LANGUAGES = (
-    LANG_C, LANG_CPP, LANG_PASCAL, LANG_JAVA, LANG_PYTHON, LANG_HS, LANG_RUST,
-    LANG_C_SHARP
+    LANG_C, LANG_CPP, LANG_CPP17, LANG_PASCAL, LANG_JAVA,
+    LANG_PYTHON, LANG_HS, LANG_RUST, LANG_C_SHARP
 )
 
 ALL_TESTS = [
@@ -417,5 +420,12 @@ ALL_TESTS = [
          task=batch_fileio, filenames=['write-big-fileio.%l'],
          languages=(LANG_C,),
          checks=[CheckOverallScore(0, 100)]),
+
+    # Language-specific tests
+
+    Test('correct-stdio-cxx17',
+         task=batch_stdio, filenames=['correct-stdio-cxx17.%l'],
+         languages=(LANG_CPP17,),
+         checks=[CheckOverallScore(100, 100)]),
 
 ]
