3ed1e4e72c40d5e3c7fc1a350e7bf594aab470bd
Vytis Banaitis <vytis.banaitis@gmail.com>
2024-03-07 23:51:03 +0200

    Make oom-heap.c test work with newer GCC again
    It seems that writing only zeroes can be optimized away.

--- cmstestsuite/code/oom-heap.c
+++ cmstestsuite/code/oom-heap.c
@@ -9,7 +9,7 @@ int main() {
     // If we don't do this cycle, the compiler is smart enough not to
     // map the array into resident memory.
     for (i = 0; i < 128 * 1024 * 1024 / sizeof(int); i++) {
-      big[i] = 0;
+      big[i] = 1;
     }
     scanf("%d", &big[10000]);
     printf("correct %d\n", big[10000]);
