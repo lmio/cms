50a83a942761f3099c77763fa6559c63a96a3084
Vytis Banaitis <vytis.banaitis@gmail.com>
2026-07-08 01:36:11 +0300

    Polygon: allow additional header files for checker compilation

--- cmscontrib/loaders/polygon.py
+++ cmscontrib/loaders/polygon.py
@@ -250,12 +250,14 @@ class PolygonTaskLoader(TaskLoader):
                     os.path.dirname(checker_src), "checker")
                 testlib_path = "/usr/local/include/cms"
                 testlib_include = os.path.join(testlib_path, "testlib.h")
+                extra_include = os.path.join(self.path, "files")
                 if not config.installed:
                     testlib_path = os.path.join(os.path.dirname(__file__),
                                                 "polygon")
                 code = subprocess.call(["g++", "-x", "c++", "-O2", "-static",
                                         "-DCMS", "-I", testlib_path,
                                         "-include", testlib_include,
+                                        "-iquote", extra_include,
                                         "-o", checker_exe, checker_src])
                 if code != 0:
                     logger.critical("Could not compile checker")
